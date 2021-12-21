979b4cb6aa703bb9941580d28aed60a6a2c2dd70
aprentix <raqueldiazchavez9@gmail.com>
2021-12-21 17:53:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="estacion">
+        <div className="texto">
+            A la primavera
+            <p>
+              Por todos los amaneceres claros que nos regaló en este 2021. 
+            </p>
+        </div>
+      </div>
+      <div className="estacion">
+        <div className="texto">
+              Al verano
+            <p>
+              Por traer su calidez a nuestros corazones. 
+            </p>
+        </div>
+      </div>
+      <div className="estacion">
+        <div className="texto">
+              Al otoño
+            <p>
+              Por teñir al mundo de colores. 
+            </p>
+        </div>
+      </div>
+      <div className="estacion">
+        <div className="centrar">
+          <header className="header">
+              Feliz Navidad!!
+          </header>
+        </div>
+      </div>
+      <div className="estacion">
+        <div className="texto">
+            Os deseamos un 
+            <br></br>
+            Feliz 2022
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+
+export default App;
